b2034f3f1fe2c368b9d55b5e2987e4c3d0665115
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-08-18 12:12:19 -0300

    feat: Refactor cart service functions and improve error handling

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,7 +1,54 @@
-async function addItem(Usercart) {}
+function addItem(userCart, item) {
+  if (!userCart || !item) {
+    throw new Error("Invalid parameters");
+  }
+  userCart.push(item);
+}
 
-async function deleteItem(userCart, name) {}
+function removeItem(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name);
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  }
+}
 
-async function removeItem(userCart, index) {}
+function decreaseItemQuantity(userCart, item) {
+  const indexFound = userCart.findIndex((p) => p.name === item.name);
 
-async function calculateTotal(userCart) {}
+  if (indexFound === -1) {
+    return "Item não encontrado";
+  }
+
+  if (userCart[indexFound].quantity > 1) {
+    userCart[indexFound].quantity -= 1;
+  } else {
+    userCart.splice(indexFound, 1);
+  }
+}
+
+function displayCart(userCart) {
+  if (!userCart) {
+    throw new Error("Invalid parameter");
+  }
+  return userCart.map((item, index) => {
+    return `${index + 1} - ${item.name} - ${item.price} - ${
+      item.quantity
+    } | Subtotal: ${item.subtotal()}`;
+  });
+}
+
+function calculateTotal(userCart) {
+  if (!userCart) {
+    throw new Error("Invalid parameter");
+  }
+  const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+  return result;
+}
+
+export {
+  addItem,
+  calculateTotal,
+  removeItem,
+  decreaseItemQuantity,
+  displayCart,
+};
